8aeed64f2f1a97a45bfce112180b15bce3b7e9ac
aarti-Rawat07 <ra8669134@gmail.com>
2026-04-23 14:05:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,25 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnet from default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Create EC2 instance
 resource "aws_instance" "demo" {
-  ami           = "ami-05d2d839d4f73aafb"
+  ami           = "ami-05d2d839d4f73aafb"   # Amazon Linux / Ubuntu (region-specific)
   instance_type = "t2.micro"
 
-  subnet_id = "subnet-0b3d913d19707b019"
+  subnet_id = data.aws_subnets.default.ids[0]
 
   tags = {
     Name = "Jenkins-Terraform-Demo"
