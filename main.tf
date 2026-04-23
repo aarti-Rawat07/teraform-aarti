fd3802c3f22c2ca47590bf6ef636e0fb468b15b2
aarti-Rawat07 <ra8669134@gmail.com>
2026-04-23 13:39:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_instance" "demo" {
   ami           = "ami-05d2d839d4f73aafb"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
 
   subnet_id = "subnet-0b3d913d19707b019"
 
